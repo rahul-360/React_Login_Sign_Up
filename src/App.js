a210faf57b8efff97dae3e63624e066a0b05f3bb
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-02-12 22:06:17 +0530

    commits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+     <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
